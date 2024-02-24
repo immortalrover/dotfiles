eb7a3158f340803ed89ac4b799249e675ed92a40
immortalrover <immortalrover@protonmail.com>
2024-02-25 01:37:18 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,12 @@ end
 
 set -x EDITOR vim
 set -x GPG_TTY $(tty)
+
+function ya
+    set tmp (mktemp -t "yazi-cwd.XXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
